1acc28012ac5ecf6e46713b2d4329f598e69e433
Brandon Wade <bwade.dev@gmail.com>
2025-03-26 23:29:45 -0700

    added formatted index to file name generation

--- src/CityOfBinds/rotatingbind.py
+++ src/CityOfBinds/rotatingbind.py
@@ -19,10 +19,16 @@ class RotatingBind():
     ### Methods
     def publish_bind_rotation_files(self, path: str = '', file_prefix: str = ''):
         """Write all the binds to the file."""
+        file_count = len(self._bind_list)
+        index_width = len(str(file_count - 1))  # Calculate the number of digits in the highest index
+
         for file_index, bind in enumerate(self._bind_list):
-            next_file_index = (file_index + 1) % len(self._bind_list)
-            bind.slash_commands += f"{BIND_LOAD_FILE} {path}{file_prefix}{next_file_index}.txt"
-            bind_file = BindFile(filename=f"{file_prefix}{file_index}.txt", binds=[bind])
+            next_file_index = (file_index + 1) % file_count
+            formatted_file_index = str(file_index).zfill(index_width)  # Format with leading zeroes
+            formatted_next_file_index = str(next_file_index).zfill(index_width)  # Format next index
+
+            bind.slash_commands += f"{BIND_LOAD_FILE} {path}{file_prefix}{formatted_next_file_index}.txt"
+            bind_file = BindFile(filename=f"{file_prefix}{formatted_file_index}.txt", binds=[bind])
             bind_file.write_to_file(path=path)
 
     ### Error Checking/Validation
