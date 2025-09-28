25dc0274dad730a0ee67ffb9164043ac8447fa41
Brandon Wade <bwade.dev@gmail.com>
2025-09-27 22:02:47 -0700

    adding silence flag

--- src/CityOfBinds/rotatingbind.py
+++ src/CityOfBinds/rotatingbind.py
@@ -3,10 +3,13 @@ from CityOfBinds.bindfile import BindFile
 from CityOfBinds.slashcommands import SlashCommands
 
 class RotatingBind():
-    def __init__(self, bind_list: list[Bind]):
+    def __init__(self, bind_list: list[Bind], is_silent: bool = False):
         self._bind_list = bind_list
-        self.bind_list = bind_list
+        self._is_silent = is_silent
 
+        self.bind_list = bind_list
+        self.is_silent = is_silent
+        
     ### Properties
     @property
     def bind_list(self) -> list[Bind]:
@@ -16,6 +19,18 @@ class RotatingBind():
     def bind_list(self, bind_list: list[Bind]):
         self._throw_error_on_invalid_bind_list(bind_list)
         self._bind_list = bind_list
+
+    @property
+    def is_silent(self) -> bool:
+        return self._is_silent
+    
+    @is_silent.setter
+    def is_silent(self, is_silent: bool):
+        self._is_silent = is_silent
+        if is_silent:
+            self._bind_load_file_command = SlashCommands.BIND_LOAD_FILE_SILENT
+        else:
+            self._bind_load_file_command = SlashCommands.BIND_LOAD_FILE
 
     ### Methods
     def publish_rotating_bind_files(self, path: str = '', file_prefix: str = ''):
@@ -28,7 +43,7 @@ class RotatingBind():
             formatted_file_index = str(file_index).zfill(index_width)  # Format with leading zeroes
             formatted_next_file_index = str(next_file_index).zfill(index_width)  # Format next index
 
-            bind.slash_commands.append(f"{SlashCommands.BIND_LOAD_FILE} {path}/{file_prefix}{formatted_next_file_index}.txt")
+            bind.slash_commands.append(f"{self._bind_load_file_command} {path}/{file_prefix}{formatted_next_file_index}.txt")
             bind_file = BindFile(filename=f"{file_prefix}{formatted_file_index}.txt", binds=[bind])
             bind_file.write_to_file(path=path)
 
