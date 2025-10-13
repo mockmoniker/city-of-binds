e2586bfe85e62b2cbfa1196b9a1cdea68b57408a
Brandon Wade <bwade.dev@gmail.com>
2025-10-12 19:41:09 -0700

    Refactor BindFile class to support both Bind and Comment types; remove BindGroup class

--- src/CityOfBinds/bindfile.py
+++ src/CityOfBinds/bindfile.py
@@ -1,16 +1,17 @@
+from typing import Union
 from CityOfBinds.binds import Bind
-from CityOfBinds.comments import CommentBanner
+from CityOfBinds.comments import Comment
+
+class BindFileConstants:
+    EXTENSION = ".txt"
 
 class BindFile:
-    def __init__(self, filename: str, binds: list[Bind] = [], comment_banner_text: str = ""):
+    def __init__(self, filename: str, content_list: list[Union[Bind, Comment]] = None):
         """Initialize the bind file with a filename."""
         self._filename = None
-        self._binds = None
-        self._comment_banner = None
 
         self.filename = filename
-        self.binds = binds
-        self.comment_banner = comment_banner_text
+        self.contensts = content_list if content_list is not None else []
 
     ### Properties
     @property
@@ -22,46 +23,41 @@ class BindFile:
         self._throw_error_on_invalid_filename(filename)
         self._filename = filename
 
-    @property
-    def comment_banner(self) -> str:
-        return self._comment_banner.comment_banner_string
+    ### Methods
+    def preview(self) -> str:
+        """Return the contents of the bind file as a string."""
+        return self._build_file_contents()
 
-    @comment_banner.setter
-    def comment_banner(self, comment_banner: str):
-        self._comment_banner = CommentBanner(comment_banner)
+    def write_to_path(self, path: str = ""):
+        """Write all the binds to the file after validation."""
+        self.validate_contents()
+        with open(path / self.filename + BindFileConstants.EXTENSION, 'w') as file:
+            file.write(self._build_file_contents())
 
-    @property
-    def binds(self) -> list[Bind]:
-        return self._binds
-    
-    @binds.setter
-    def binds(self, binds: list[Bind]):
-        self._throw_error_on_invalid_binds(binds)
-        self._binds = binds
+    def validate_contents(self):
+        for content in self.contents:
+            if not isinstance(content, (Bind, Comment)):
+                raise ValueError("All contents must be instances of Bind or Comment")
+            if isinstance(content, Bind):
+                content.validate()
 
-    def write_to_file(self, path: str = ""):
-        """Write all the binds to the file."""
-        with open(path / self.filename, 'w') as file:
-            file.write(self._build_message_string())
-            for bind in self._binds:
-                file.write(bind.bind_string + "\n")
-
-    def _build_message_string(self) -> str:
-        """Helper function to build a message string from the binds."""
-        if self.comment_banner:
-            comment_banner_lines = [comment_banner_line.strip() for comment_banner_line in self.comment_banner.split('\n')]
-            max_line_length = max(len(line) for line in comment_banner_lines)
-            border = '-' * (max_line_length + 4)
-            return f"#\n# {self.comment_banner}\n#\n"
-        return ""
+    ### Helpers
+    def _build_file_contents(self) -> str:
+        file_contents = ""
+        for content in self.contents:
+            if isinstance(content, Comment):
+                file_contents += content.comment_string + "\n"
+            elif isinstance(content, Bind):
+                file_contents += content.bind_string + "\n"
+        return file_contents.strip()
 
     ### Error Checking/Validation
     def _throw_error_on_invalid_filename(self, filename: str):
         if not filename:
             raise ValueError("Filename cannot be empty")
-        if not filename.endswith(".txt"):
-            raise ValueError(f"Invalid filename {filename}. Filename must end with .txt")
+        if '.' in filename:
+            raise ValueError("Filename should not contain an extension")
 
     def __repr__(self):
-        """Optional: Represent the BindFile with its filename and binds."""
-        return f"BindFile(filename={self.filename}, binds={self.binds})"
+        """Optional: Represent the BindFile with its filename and contents."""
+        return f"BindFile(filename={self.filename}, contents={self.contents})"
