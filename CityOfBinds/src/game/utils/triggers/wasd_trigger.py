3b62f881087e2dbb28cb6703836365eb9f2272fd
Brandon Wade <bwade.dev@gmail.com>
2026-01-01 21:16:21 -0800

    Refactor WASD key mappings to use centralized WASDMaps class

--- CityOfBinds/src/game/utils/triggers/wasd_trigger.py
+++ CityOfBinds/src/game/utils/triggers/wasd_trigger.py
@@ -1,17 +1,11 @@
 from typing import Set
 
+from ....configs.maps import WASDMaps
 from .trigger import _Trigger
 
 
 class _WASDTrigger(_Trigger):
-    KEY_TO_DIRECTION_MAP = {
-        "W": "forward",
-        "A": "left",
-        "S": "backward",
-        "D": "right",
-        "SPACE": "up",
-    }
-    VALID_TRIGGER_KEYS: Set[str] = set(KEY_TO_DIRECTION_MAP.keys())
+    VALID_TRIGGER_KEYS: Set[str] = set(WASDMaps.KEY_TO_DIRECTION_MAP.keys())
 
     # region Validation and Error Checking
     def _throw_invalid_key_error(self, key: str):
